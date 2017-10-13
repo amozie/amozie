4e44961de3305999ac0de981ccd01ccb58f23e4c
kami <xchick@qq.com>
2017-10-13 17:49:35 +0800

    累积更新

--- stockzie/strategy/TechStrategy.py
+++ stockzie/strategy/TechStrategy.py
@@ -18,6 +18,8 @@ from datetime import datetime
 import stockzie as sz
 from stockzie.technique import Technique
 
+TOL_ERR = 0.001
+
 
 class StrategyError(Exception):
     def __init__(self, message):
@@ -25,17 +27,15 @@ class StrategyError(Exception):
         self.message = message
 
 
-class TechStrategy():
+class TechStrategy:
     def __init__(self):
         self.__last_day = None
         self.__techniques = {}
-        self.__trading_simple = {}
+        self.__trading = Trading(1.0)
 
         self.iter = None
         self.data_hist = None
         self.data_i = None
-        self.is_position = False
-
 
     ####################################
 
@@ -115,105 +115,109 @@ class TechStrategy():
                 self._before_trading(data)
 
             self._handle_trading(data)
+            self.__trading.calc_trading_bar(self.data_i, self.iter)
 
             if date.hour == 0 or date.hour == 15:
+                self.__trading.trading_bar_day()
                 self._after_trading(data)
 
             self.__last_day = date.day
 
         self._end_trading(data)
 
-        self.__calc_trading_simple(data)
+        self.__calc_trading_tech()
 
         return self.__techniques
 
-    def __trade_simple(self, price, buy):
-        trade = self.__trading_simple.get(self.iter)
-        if trade is None:
-            self.__trading_simple[self.iter] = [
-                {
-                    'buy': buy,
-                    'price': price
-                }
-            ]
-        else:
-            trade.append(
-                {
-                    'buy': buy,
-                    'price': price
-                }
-            )
+    def buy_soft_percentage(self, price, percentage):
+        self.__trading.buy_soft_percentage(price, percentage)
 
-    def buy_simple(self, price):
-        self.__trade_simple(price, True)
+    def sell_soft_percentage(self, price, percentage):
+        self.__trading.sell_soft_percentage(price, percentage)
 
-    def sell_simple(self, price):
-        self.__trade_simple(price, False)
-
-    def __calc_trading_simple(self, data, stop=0.1):
-        total = 1
-        buy_date = None
-        total_trading = []
-        stop_price = None
-        for i in range(data.shape[0]):
-            high = data.high.iloc[i]
-            low = data.low.iloc[i]
-            trade = self.__trading_simple.get(i)
-            if stop_price is not None:
-                if low <= stop_price:
-                    buy_date = None
-                    last_price = data.iloc[i - 1].close
-                    total = total * stop_price / last_price
-                    stop_price = None
-            if trade is not None:
-                datestr = data.iloc[i].date.split()[0]
-                try:
-                    date = datetime.strptime(datestr, '%Y-%m-%d')
-                except ValueError:
-                    raise StrategyError('cannot parse datestr to datetime')
-                for j in trade:
-                    if j['buy']:
-                        if buy_date is None:
-                            buy_price = j['price']
-                            if low <= buy_price <= high:
-                                buy_date = date
-                                cur_price = data.iloc[i].close
-                                total = total * cur_price / buy_price
-                                if stop > 0:
-                                    stop_price = high * (1 - stop)
-                    else:
-                        if buy_date is not None and date > buy_date:
-                            sell_price = j['price']
-                            if low <= sell_price <= high:
-                                buy_date = None
-                                last_price = data.iloc[i-1].close
-                                total = total * sell_price / last_price
-                                if stop > 0:
-                                    stop_price = None
-            else:
-                if buy_date is not None:
-                    last_price = data.iloc[i - 1].close
-                    cur_price = data.iloc[i].close
-                    total = total * cur_price / last_price
-
-            total_trading.append(total)
-        self._add_technique('ASSET', total_trading, 1, twin=True)
+    def __calc_trading_tech(self):
+        self._add_technique('ASSET', trading_tech['ASSET'], 1, twin=True)
 
 
-class Trading():
-    def __init__(self, cash):
-        self.__cash = cash
-        self.__market_value = 0
-        self.__total_value = cash
+class Trading:
+    def __init__(self, cash, stop=0.1):
         self.__commision = 0.0005
         self.__stamp_tax = 0.001
+        self.__stop = stop
 
-        self.__total = 1
-        self.__is_position = False
+        self.__cash = cash
+        self.__avail_pos = 0.0
+        self.__frozen_pos = 0.0
+        self.__position = 0.0
+        self.__pos_price = 0.0
+        self.__equity = 0.0
+        self.__total = cash
+        self.__last_max_price = 0.0
 
-    def buy_all(self, price):
-        pass
+        self.__trading_bar = []
 
-    def sell_all(self, price):
-        pass
+        self.total_list = []
+        self.buy_list = []
+        self.buy_x = []
+        self.sell_list = []
+        self.sell_x = []
 
+    def buy_soft_percentage(self, price, percentage=1.0):
+        self._add_trading_bar(price, True, self.__cash * percentage / price)
+
+    def sell_soft_percentage(self, price, percentage=1.0):
+        self._add_trading_bar(price, False, self.__position * percentage)
+
+    def _add_trading_bar(self, price, buy, quantity):
+        self.__trading_bar.append(
+            {
+                'buy': buy,
+                'price': price,
+                'quantity': quantity
+            }
+        )
+
+    def update_trading_value(self, price):
+        # self.__cash
+        # self.__avail_pos
+        # self.__frozen_pos
+        self.__position = self.__avail_pos + self.__frozen_pos
+        self.__pos_price = price
+        self.__equity = self.__pos_price * self.__position
+        self.__total = self.__equity + self.__cash
+
+    def calc_trading_bar(self, data_i, itr, stop=0.1):
+        low = data_i.low
+        high = data_i.high
+        close = data_i.close
+        for trade in self.__trading_bar:
+            buy = trade['buy']
+            price = trade['price']
+            quantity = trade['quantity']
+            if low <= price <= high:
+                if buy:
+                    if self.__cash + TOL_ERR >= price * quantity:
+                        self.__cash -= price * quantity
+                        self.__frozen_pos += quantity
+                        self.buy_list.append(price)
+                        self.buy_x.append(itr)
+                else:
+                    if self.__avail_pos + TOL_ERR >= quantity:
+                        self.__cash += price * quantity
+                        self.__avail_pos -= quantity
+                        self.sell_list.append(price)
+                        self.sell_x.append(itr)
+                self.update_trading_value(price)
+        self.update_trading_value(close)
+        self.__trading_bar.clear()
+        self._trading_stop(high)
+        self.total_list.append(self.__total)
+
+    def trading_bar_day(self):
+        self.__avail_pos += self.__frozen_pos
+        self.__frozen_pos = 0
+
+    def _trading_stop(self, high):
+        if self.__avail_pos > TOL_ERR:
+            self.__last_max_price = max(high, self.__last_max_price)
+            self.sell_soft_percentage(self.__last_max_price * (1 - self.__stop), self.__avail_pos)
